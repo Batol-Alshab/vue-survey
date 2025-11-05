2fbf342685f47f5d1932c69b468eecb91feb472a
batol-alshab <batolsh2003@gmail.com>
2025-11-05 11:25:44 +0300

    routers and logout

---
+++ src/router/index.js
@@ -0,0 +1,69 @@
+import { createRouter } from "vue-router";
+import { createWebHistory } from "vue-router";
+import DefaultLayout from "../components/DefaultLayout.vue";
+import Dashboard from "../views/Dashboard.vue";
+import login from "../views/Login.vue";
+import register from "../views/Register.vue";
+import surveys from "../views/Surveys.vue";
+import store from "../store";
+import AuthLayout from "../components/AuthLayout.vue";
+
+const routes = [
+  {
+    path: "/",
+    derirted: "Dashboard",
+    name: "Dashboard",
+    component: DefaultLayout,
+    meta: { requiresAuth: true },
+    children: [
+      {
+        path: "/Dashboard",
+        name: "Dashboard",
+        component: Dashboard,
+      },
+      {
+        path: "/surveys",
+        name: "Surveys",
+        component: surveys,
+      },
+    ],
+  },
+  {
+    path: "/auth",
+    redirect: "/login",
+    name: "Auth",
+    component: AuthLayout,
+    meta: { isGuest: true },
+    children: [
+      {
+        path: "/register",
+        name: "Register",
+        component: register,
+      },
+      {
+        path: "/login",
+        name: "Login",
+        component: login,
+      },
+    ],
+  },
+];
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth && !store.state.user.token) {
+    next({ name: "Login" });
+  } else if (
+    store.state.user.token &&
+    (to.meta.isGuest)
+    // (to.name === "Login" || to.name === "Register")
+  ) {
+    next({ name: "Dashboard" });
+  } else {
+    next();
+  }
+});
+
+export default router;
---
+++ src/views/Register.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <div class="flex min-h-full flex-1 flex-col justify-center items-center">
+        <img
+          class="mx-auto h-10 w-auto"
+          src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+          alt="Your Company"
+        />
+      </div>
+      <h2
+        class="pb-12 mt-10 text-center text-2xl/9 font-bold tracking-tight text-white"
+      >
+        Register
+      </h2>
+    </div>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" action="#" method="POST">
+        <div class="pb-6">
+          <label for="fulname" class="block text-sm/6 font-medium text-gray-100 pb-3"
+            >Full name</label
+          >
+          <div class="mt-2">
+            <input
+              type="twext"
+              name="name"
+              id="name"
+              autocomplete="name"
+              required
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+        <div class="pb-6">
+          <label for="email" class="block text-sm/6 font-medium text-gray-100 pb-3"
+            >Email address</label
+          >
+          <div class="mt-2">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              required
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div class="pb-6">
+          <div class="flex items-center justify-between pb-3">
+            <label
+              for="password"
+              class="block text-sm/6 font-medium text-gray-100"
+              >Password</label
+            >
+          </div>
+          <div class="mt-2">
+            <input
+              type="password"
+              name="password"
+              id="password"
+              autocomplete="current-password"
+              required
+              class="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+            />
+          </div>
+        </div>
+
+        <div class="pb-8">
+          <button
+            type="submit"
+            class="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            Sign up
+          </button>
+        </div>
+      </form>
+
+      <p class="mt-10 text-center text-sm/6 text-gray-400">
+        have an account?
+        {{ " " }}
+        <router-link
+          :to="{ name: 'Login' }"
+          class="font-semibold text-indigo-400 hover:text-indigo-300"
+          >login to your account</router-link
+        >
+      </p>
+    </div>
+  </div>
+</template>
