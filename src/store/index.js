196d1d49f97afc1cc1dc5ed966b831341344d807
batol-alshab <batolsh2003@gmail.com>
2025-11-06 14:16:50 +0300

    Survey Card with Tailwindcss

--- src/store/index.js
+++ src/store/index.js
@@ -211,6 +211,215 @@ const tmpSurveys = [
       },
     ],
   },
+   {
+    id: 3,
+    title: "My First Survey",
+    slug: "my-first-survey",
+    status: "draft",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNOUXBO27g82sqvcZu57eEDH_R7vxqh1XDUimEBM99toVAfBbyuStm_7Tr5VIZjPFKmOZyscQFHEdXb2S69eZIG3xIRvHVACZBNWEmE6Xi&s=10",
+    description: "This is a test survey",
+    expire_date: "2025-12-31T23:59:59",
+    created_at: "2025-11-06T12:00:00",
+    updated_at: "2025-11-06T12:00:00",
+    questions: [
+      {
+        id: 1,
+        type: "select",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 2,
+        type: "checbox",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 3,
+        type: "checbox",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 4,
+        type: "radio",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 5,
+        type: "checbox",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 6,
+        type: "text",
+        questions: "from which country are you?",
+        description: null,
+        data: {},
+      },
+
+      {
+        id: 7,
+        type: "textarea",
+        questions: "from which country are you?",
+        description: null,
+        data: {},
+      },
+    ],
+  },
+
+  {
+    id: 4,
+    title: "My First Survey",
+    slug: "my-first-survey",
+    status: "draft",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNOUXBO27g82sqvcZu57eEDH_R7vxqh1XDUimEBM99toVAfBbyuStm_7Tr5VIZjPFKmOZyscQFHEdXb2S69eZIG3xIRvHVACZBNWEmE6Xi&s=10",
+    description: '',
+    expire_date: "2025-12-31T23:59:59",
+    created_at: "2025-11-06T12:00:00",
+    updated_at: "2025-11-06T12:00:00",
+    questions: [
+      {
+        id: 1,
+        type: "select",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 2,
+        type: "checbox",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 3,
+        type: "checbox",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 4,
+        type: "radio",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 5,
+        type: "checbox",
+        questions: "from which country are you?",
+        description: null,
+        data: {
+          options: [
+            { uuid: "550e8400-e29b-41d4-a716-446655440000", text: "USA" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440001", text: "Canada" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440002", text: "UK" },
+            { uuid: "550e8400-e29b-41d4-a716-446655440003", text: "Australia" },
+          ],
+        },
+      },
+
+      {
+        id: 6,
+        type: "text",
+        questions: "from which country are you?",
+        description: null,
+        data: {},
+      },
+
+      {
+        id: 7,
+        type: "textarea",
+        questions: "from which country are you?",
+        description: null,
+        data: {},
+      },
+    ],
+  },
 ];
 const store = createStore({
   state: {
